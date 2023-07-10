b1ca3b670a5874a221ca96ccded2bec8a446dff7
Nahdian <nahdian7@gmail.com>
2023-07-10 20:36:18 +0700

    send verification code to email and update verification code

--- controllers/users_controllers.js
+++ controllers/users_controllers.js
@@ -1,5 +1,6 @@
 const Joi = require('joi');
 const bcrypt = require('bcrypt');
+const nodemailer = require('nodemailer');
 const connection = require('../connections/mysql_connection');
 
 // @desc Account Register
@@ -25,9 +26,78 @@ exports.userRegister = (req, res) => {
     const createConnection = connection.createConnection();
     const sqlCheckEmail = `SELECT email FROM users WHERE email = '${value.email}'`;
 
+    // ==== SEND EMAIL VERIFICATION CODE ====
+    function sendEmailVerification(email, verificationCode) {
+        try {
+            nodemailer.createTestAccount((err, account) => {
+                if (err) {
+                    res.status(300).send('Failed to create a testing account.');
+                };
+
+                const transporter = nodemailer.createTransport({
+                    host: account.smtp.host,
+                    port: account.smtp.port,
+                    secure: account.smtp.secure,
+                    auth: {
+                        user: account.user,
+                        pass: account.pass,
+                    },
+                });
+
+                const mailMessage = {
+                    from: account.smtp.host,
+                    to: email,
+                    subject: 'Email Verification',
+                    text: `Your verification code is: ${verificationCode}`,
+                };
+
+                transporter.sendMail(mailMessage, (err, info) => {
+                    if (err) {
+                        res.status(300).send('Fail to send email verification');
+                    }
+
+                    console.log('- Preview URL: ' + nodemailer.getTestMessageUrl(info));
+                });
+            });
+
+        } catch (error) {
+            console.log(`- Error ouccured: ${error} `);
+        };
+    };
+
     function checkEmailResult(result) {
         if (result === 1) {
-            res.status(400).send('Email is already exists!');
+            const sqlGetIsVerified = `SELECT isVerified FROM users WHERE email = '${value.email}'`;
+
+            createConnection.query(sqlGetIsVerified, (error, results, fields) => {
+                if (error) {
+                    res.send(error);
+                }
+
+                const isVerified = results[0].isVerified;
+
+                const generateLength = Math.floor(Math.random() * 10000);
+                const verificationCode = generateLength.toString().padStart(4, '0');
+
+                if (isVerified === 0) {
+                    const sqlUpdateVerificationCode = `UPDATE users SET verificationCode = ${verificationCode} WHERE email = '${value.email}'`;
+
+                    createConnection.query(sqlUpdateVerificationCode, (error) => {
+                        if (error) {
+                            res.send(error);
+                        }
+
+                        res.status(200).json({
+                            message: 'Update verification code!',
+                            accepted: value.email
+                        });
+                    });
+                }
+
+                if (isVerified === 1) {
+                    res.status(400).send('Email is already exists!');
+                }
+            });
         };
 
         if (result > 1) {
@@ -35,20 +105,25 @@ exports.userRegister = (req, res) => {
         };
 
         if (result === 0) {
+            const generateLength = Math.floor(Math.random() * 10000);
+            const verificationCode = generateLength.toString().padStart(4, '0');
+
+            sendEmailVerification(value.email, verificationCode);
+
             // ==== HASH PASSWORD ====
             const hashPassword = bcrypt.hashSync(value.password, 10);
 
             // ==== POST DATA =====
-            const sqlPostData = `INSERT INTO users(username, email, password) values('${value.username}','${value.email}','${hashPassword}')`;
+            const sqlPostData = `INSERT INTO users(username, email, password, isVerified, verificationCode) values('${value.username}','${value.email}','${hashPassword}', '0', ${verificationCode})`;
             createConnection.query(sqlPostData, (error, result, query) => {
                 if (error) {
                     res.status(400);
                     throw new Error(`Error when upload data to db: ${error}`);
-                }
+                };
 
                 res.status(200).json({
                     message: 'Register success!',
-                    value: result
+                    accepted: value.email
                 });
             });
 
@@ -63,6 +138,15 @@ exports.userRegister = (req, res) => {
         checkEmailResult(rowDataPacket);
     });
 };
+
+// @desc Verification Email
+// @route POST - /users/register/email-verification
+// @access public
+exports.registerEmailVerification = (req, res) => {
+
+
+};
+
 
 // @desc
 // @route
--- routes/users_routes.js
+++ routes/users_routes.js
@@ -2,6 +2,8 @@ const router = require('express').Router();
 const usersControllers = require('../controllers/users_controllers');
 
 router.post('/register', usersControllers.userRegister);
+router.post('/register/email-verification', usersControllers.registerEmailVerification);
+
 router.get('/login', usersControllers.userLogin);
 
 module.exports = router;
